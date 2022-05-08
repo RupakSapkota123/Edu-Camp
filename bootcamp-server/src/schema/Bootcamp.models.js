0e0c515651e3538f02d77152c6ec17efdc05597e
RupakSapkota123 <rupaksapkota2000@gmail.com>
2022-05-08 07:38:56 +0545

    bootcamp client initilized and bootcamps Joi validation

--- bootcamp-server/src/schema/Bootcamp.models.js
+++ bootcamp-server/src/schema/Bootcamp.models.js
@@ -11,93 +11,108 @@ const BootcampSchema = new mongoose.Schema({
     maxlength: [50, 'Name can not be more than 50 characters'],
     minlength: [5, 'Name can not be less than 5 characters'],
   },
-  //     slug: String,
-  //     description: {
-  //       type: String,
-  //       required: [true, 'Please add a description'],
-  //       maxlength: [500, 'Description can not be more than 500 characters'],
-  //       minlength: [5, 'Description can not be less than 5 characters'],
-  //     },
-  //     website: {
-  //       type: String,
-  //       match: [
-  //         /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-  //         'Please use a valid URL with HTTP or HTTPS',
-  //       ],
-  //     },
-  //     phone: {
-  //       type: String,
-  //       maxlength: [20, 'Phone number can not be longer than 20 characters'],
-  //     },
-  //     email: {
-  //       type: String,
-  //       match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid email'],
-  //     },
-  //     address: {
-  //       type: String,
-  //       required: [true, 'Please add an address'],
-  //     },
-  //     location: {
-  //       // GeoJSON Point
-  //       type: {
-  //         type: String,
-  //         enum: ['Point'],
-  //       },
-  //       coordinates: {
-  //         type: [Number],
-  //         index: '2dsphere',
-  //       },
-  //       formattedAddress: String,
-  //       street: String,
-  //       city: String,
-  //       state: String,
-  //       zipcode: String,
-  //       country: String,
-  //     },
-  //     careers: {
-  //       // Array of strings
-  //       type: [String],
-  //       required: true,
-  //       enum: ['Web Development', 'Mobile Development', 'UI/UX', 'Data Science', 'Business', 'Other'],
-  //     },
-  //     averageRating: {
-  //       type: Number,
-  //       min: [1, 'Rating must be at least 1'],
-  //       max: [10, 'Rating must can not be more than 10'],
-  //     },
-  //     averageCost: Number,
-  //     photo: {
-  //       type: String,
-  //       default: 'no-photo.jpg',
-  //     },
-  //     housing: {
-  //       type: Boolean,
-  //       default: false,
-  //     },
-  //     jobAssistance: {
-  //       type: Boolean,
-  //       default: false,
-  //     },
-  //     jobGuarantee: {
-  //       type: Boolean,
-  //       default: false,
-  //     },
-  //     acceptGi: {
-  //       type: Boolean,
-  //       default: false,
-  //     },
-  //     createdAt: {
-  //       type: Date,
-  //       default: Date.now,
-  //     },
-  //     user: {
-  //       type: mongoose.Schema.ObjectId,
-  //       ref: 'User',
-  //       required: true,
-  //     },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    maxlength: [500, 'Description can not be more than 500 characters'],
+    minlength: [5, 'Description can not be less than 5 characters'],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/,
+      'Please use a valid URL with HTTP or HTTPS',
+    ],
+  },
+  phone: {
+    type: String,
+    maxlength: [20, 'Phone number can not be longer than 20 characters'],
+  },
+  email: {
+    type: String,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid email'],
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address'],
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point'],
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere',
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    // Array of strings
+    type: [String],
+    required: true,
+    enum: ['Web Development', 'Mobile Development', 'UI/UX', 'Data Science', 'Business', 'Other'],
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating must can not be more than 10'],
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg',
+  },
+  housing: {
+    type: Boolean,
+    default: false,
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false,
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false,
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
 });
 
 //! add plugin that converts mongoose to json
 BootcampSchema.plugin(toJSON);
 
-export default mongoose.model('Bootcamp', BootcampSchema);
+/**
+ * Check if name is taken
+ * @param {string} name - The bootcamp's name
+ *@param {ObjectId} [excludeUserId] - The id of the bootcamp to be excluded
+ *@returns {Promise<boolean>}
+ */
+BootcampSchema.statics.isNameTaken = async (name, excludeUserId) => {
+  const bootcamp = await this.findOne({ name, _id: { $ne: excludeUserId } });
+  return !!bootcamp;
+};
+
+/**
+ * @typedef {Object} Bootcamp
+ */
+const Bootcamp = mongoose.model('Bootcamp', BootcampSchema);
+export default Bootcamp;
---
+++ bootcamp-server/src/validation/bootcamp.validations.js
@@ -0,0 +1,130 @@
+import Joi from 'joi';
+import moment from 'moment';
+
+import { objectID } from './custom.validations.js';
+
+const createBootcamp = {
+  body: Joi.object().keys({
+    name: Joi.string().required('name is required'),
+    description: Joi.string().required('description is required'),
+    website: Joi.string(),
+    phone: Joi.string().max(10),
+    email: Joi.string().email(),
+    address: Joi.string().required('Please add an address'),
+    location: Joi.object()
+      .keys({
+        type: Joi.string().valid('Point'),
+        coordinates: Joi.array().items(Joi.number()),
+        formattedAddress: Joi.string(),
+        street: Joi.string(),
+        city: Joi.string(),
+        state: Joi.string(),
+        zipcode: Joi.string(),
+        country: Joi.string(),
+      })
+      .required('Please add a location'),
+  }),
+  careers: Joi.array().items(Joi.string()).required('Please add at least one career'),
+  housing: Joi.boolean().default(false),
+  averageRating: Joi.number().min(1).max(10).default(0),
+  averageCost: Joi.number().default(0),
+  photo: Joi.string().default('no-photo.jpg'),
+  jobAssistance: Joi.boolean().default(false),
+  jobGuarantee: Joi.boolean().default(false),
+  acceptGi: Joi.boolean().default(false),
+  user: objectID.required(),
+  createdAt: Joi.date().default(moment().format('YYYY-MM-DD')),
+};
+
+const getAllBootcamps = {
+  query: Joi.object().keys({
+    name: Joi.string().required('name is required'),
+    description: Joi.string().required('description is required'),
+    website: Joi.string(),
+    phone: Joi.string().max(10),
+    email: Joi.string().email(),
+    address: Joi.string().required('Please add an address'),
+    location: Joi.object()
+      .keys({
+        type: Joi.string().valid('Point'),
+        coordinates: Joi.array().items(Joi.number()),
+        formattedAddress: Joi.string(),
+        street: Joi.string(),
+        city: Joi.string(),
+        state: Joi.string(),
+        zipcode: Joi.string(),
+        country: Joi.string(),
+      })
+      .required('Please add a location'),
+  }),
+  careers: Joi.array().items(Joi.string()).required('Please add at least one career'),
+  housing: Joi.boolean().default(false),
+  averageRating: Joi.number().min(1).max(10).default(0),
+  averageCost: Joi.number().default(0),
+  photo: Joi.string().default('no-photo.jpg'),
+  jobAssistance: Joi.boolean().default(false),
+  jobGuarantee: Joi.boolean().default(false),
+  acceptGi: Joi.boolean().default(false),
+  user: objectID.required(),
+  createdAt: Joi.date().default(moment().format('YYYY-MM-DD')),
+  limit: Joi.number().min(1).default(10),
+  page: Joi.integer().min(1).default(1),
+};
+
+const getSingleBootcamp = {
+  params: Joi.object().keys({
+    bootcampID: Joi.string().required().custom(objectID),
+  }),
+};
+
+const updateBootcampById = {
+  params: Joi.object().keys({
+    bootcampID: Joi.string().required().custom(objectID),
+  }),
+  body: Joi.object()
+    .keys({
+      name: Joi.string().required('name is required'),
+      description: Joi.string().required('description is required'),
+      website: Joi.string(),
+      phone: Joi.string().max(10),
+      email: Joi.string().email(),
+      address: Joi.string().required('Please add an address'),
+      location: Joi.object()
+        .keys({
+          type: Joi.string().valid('Point'),
+          coordinates: Joi.array().items(Joi.number()),
+          formattedAddress: Joi.string(),
+          street: Joi.string(),
+          city: Joi.string(),
+          state: Joi.string(),
+          zipcode: Joi.string(),
+          country: Joi.string(),
+        })
+        .required('Please add a location'),
+      careers: Joi.array().items(Joi.string()).required('Please add at least one career'),
+      housing: Joi.boolean().default(false),
+      averageRating: Joi.number().min(1).max(10).default(0),
+      averageCost: Joi.number().default(0),
+      photo: Joi.string().default('no-photo.jpg'),
+      jobAssistance: Joi.boolean().default(false),
+      jobGuarantee: Joi.boolean().default(false),
+      acceptGi: Joi.boolean().default(false),
+      user: objectID.required(),
+      createdAt: Joi.date().default(moment().format('YYYY-MM-DD')),
+    })
+    .min(1),
+};
+
+const deleteBootcampById = {
+  params: Joi.object().keys({
+    bootcampID: Joi.string().required().custom(objectID),
+  }),
+};
+
+export default {
+  createBootcamp,
+  getAllBootcamps,
+  getSingleBootcamp,
+  updateBootcampById,
+  deleteBootcampById,
+};
