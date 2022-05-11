917f3ec1fda8b174fe1d0738b5ca8ebd363214a7
RupakSapkota123 <rupaksapkota2000@gmail.com>
2022-05-11 10:30:30 +0545

    auth reducers

---
+++ bootcamp-client/src/redux/reducers/authReducers.ts
@@ -0,0 +1,32 @@
+import { LOGIN_SUCCESS, LOGOUT_SUCCESS, REGISTER_SUCCESS, UPDATE_AUTH_PICTURE } from "constants/actionType";
+import { TAuthActionType } from "redux/actions/authActions";
+import { IUser } from "types/types";
+
+const initState: IUser = {
+	id: "",
+	email: "",
+	username: "",
+	fullName: "",
+	avatar: {}
+};
+
+// eslint-disable-next-line @typescript-eslint/default-param-last
+const authReducer = (state = initState, action: TAuthActionType) => {
+	switch (action.type) {
+		case LOGIN_SUCCESS:
+			return action.payload;
+		case LOGOUT_SUCCESS:
+			return initState;
+		case REGISTER_SUCCESS:
+			return action.payload;
+		case UPDATE_AUTH_PICTURE:
+			return {
+				...state,
+				avatar: action.payload
+			};
+		default:
+			return state;
+	}
+};
+
+export default authReducer;
