51a151fc136fe41b3d71f5e3b7ddb0b8e3c98933
RupakSapkota123 <rupaksapkota2000@gmail.com>
2022-05-11 10:17:25 +0545

    auth Actions

---
+++ bootcamp-client/src/redux/actions/authActions.ts
@@ -0,0 +1,81 @@
+/* eslint-disable @typescript-eslint/consistent-type-assertions */
+import {
+	CHECK_SESSION,
+	LOGIN_START,
+	LOGIN_SUCCESS,
+	LOGOUT_START,
+	LOGOUT_SUCCESS,
+	REGISTER_START,
+	REGISTER_SUCCESS,
+	UPDATE_AUTH_PICTURE
+} from "constants/actionType";
+import { IRegister, IUser } from "types/types";
+
+export const loginStart = (email: string, password: string) =>
+	<const>{
+		type: LOGIN_START,
+		payload: {
+			email,
+			password
+		}
+	};
+
+export const loginSuccess = (user: IUser) =>
+	<const>{
+		type: LOGIN_SUCCESS,
+		payload: {
+			user
+		}
+	};
+
+export const logoutStart = (callback?: () => void) =>
+	<const>{
+		type: LOGOUT_START,
+		payload: { callback }
+	};
+
+export const logoutSuccess = () =>
+	<const>{
+		type: LOGOUT_SUCCESS
+	};
+
+export const registerStart = ({ email, password, username }: IRegister) =>
+	<const>{
+		type: REGISTER_START,
+		payload: {
+			email,
+			password,
+			username
+		}
+	};
+
+export const registerSuccess = (user: IUser) =>
+	<const>{
+		type: REGISTER_SUCCESS,
+		payload: {
+			user
+		}
+	};
+
+export const checkSession = () =>
+	<const>{
+		type: CHECK_SESSION
+	};
+
+export const updateAuthPicture = (picture: Object) =>
+	<const>{
+		type: UPDATE_AUTH_PICTURE,
+		payload: {
+			picture
+		}
+	};
+
+export type TAuthActionType =
+	| ReturnType<typeof loginStart>
+	| ReturnType<typeof loginSuccess>
+	| ReturnType<typeof logoutStart>
+	| ReturnType<typeof logoutSuccess>
+	| ReturnType<typeof registerStart>
+	| ReturnType<typeof registerSuccess>
+	| ReturnType<typeof checkSession>
+	| ReturnType<typeof updateAuthPicture>;
