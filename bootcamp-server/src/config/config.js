449c027800a8928a23c86224006fe09c017cd589
RupakSapkota123 <rupaksapkota2000@gmail.com>
2022-05-11 20:31:20 +0545

    cors configuration added

--- bootcamp-server/src/config/config.js
+++ bootcamp-server/src/config/config.js
@@ -15,53 +15,44 @@ dotenv.config({ path: path.join(__dirname, './config.env') });
  */
 
 const envVarsSchema = Joi.object()
-                                                                                .keys(
-                                                                                                                                                                {
-                                                                                                                                                                                                                                                NODE_ENV: Joi.string()
-                                                                                                                                                                                                                                                                                                                                .valid(
-                                                                                                                                                                                                                                                                                                                                                                                                                'development',
-                                                                                                                                                                                                                                                                                                                                                                                                                'production',
-                                                                                                                                                                                                                                                                                                                                )
-                                                                                                                                                                                                                                                                                                                                .default(
-                                                                                                                                                                                                                                                                                                                                                                                                                'development',
-                                                                                                                                                                                                                                                                                                                                ),
-                                                                                                                                                                                                                                                PORT: Joi.number().default(
-                                                                                                                                                                                                                                                                                                                                9000,
-                                                                                                                                                                                                                                                ),
-                                                                                                                                                                                                                                                MongoDB_URL: Joi.string().description(
-                                                                                                                                                                                                                                                                                                                                'MongoDB connection URL',
-                                                                                                                                                                                                                                                ),
-                                                                                                                                                                },
-                                                                                )
-                                                                                .unknown();
+  .keys({
+    NODE_ENV: Joi.string()
+      .valid('development', 'production')
+      .default('development'),
+    PORT: Joi.number().default(9000),
+    MongoDB_URL: Joi.string().description('MongoDB connection URL'),
+    origin: Joi.string(),
+    credentials: Joi.boolean(),
+    preflightContinue: Joi.boolean(),
+  })
+  .unknown();
 
 const { value: envVars, error } = envVarsSchema
-                                                                                .prefs(
-                                                                                                                                                                {
-                                                                                                                                                                                                                                                errors: {
-                                                                                                                                                                                                                                                                                                                                label: 'key',
-                                                                                                                                                                                                                                                },
-                                                                                                                                                                },
-                                                                                )
-                                                                                .validate(
-                                                                                                                                                                process.env,
-                                                                                );
+  .prefs({
+    errors: {
+      label: 'key',
+    },
+  })
+  .validate(process.env);
 
 if (error) {
-                                                                                throw new Error(
-                                                                                                                                                                `Config validation error: ${error.message}`,
-                                                                                );
+  throw new Error(`Config validation error: ${error.message}`);
 }
 const config = {
-                                                                                env: envVars.NODE_ENV,
-                                                                                PORT: envVars.PORT,
-                                                                                mongoose: {
-                                                                                                                                                                url: envVars.MongoDB_URL,
-                                                                                                                                                                options: {
-                                                                                                                                                                                                                                                useNewUrlParser: true,
-                                                                                                                                                                                                                                                useUnifiedTopology: true,
-                                                                                                                                                                },
-                                                                                },
+  env: envVars.NODE_ENV,
+  PORT: envVars.PORT,
+  mongoose: {
+    url: envVars.MongoDB_URL,
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
+  cors: {
+    origin: envVars.CLIENT_URL,
+    credentials: true,
+    preflightContinue: true,
+  },
 };
 
 export default config;
